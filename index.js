d3faac22ec7a55767a24f5031f34d45c5387e72b
Yukti Gupta <yuktigupta95@gmail.com>
2026-07-12 18:05:03 +0530

    Add primary button ID for event listener and clean up index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-/**
-Challenge: 
+const primaryBtn = document
+  .getElementById("primary-btn")
+  .addEventListener("click", getActivity());
 
-- Add some styling! Be creative, play with layout a bit
-  (Spoiler: I'm going to use a single-column flexbox layout)
-  add some color, grab a font from Google fonts - whatever
-  you'd like!
-*/
-
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
+function getActivity() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json)
+    .then((data) => {
+      document.getElementById("activity").textContent = data.activity;
+    });
+}
