8dcf6838aeae50b8d5a3859bce649dfcfe65a4ae
Yukti Gupta <yuktigupta95@gmail.com>
2026-07-12 16:51:30 +0530

    Initialize project structure with essential files and configurations

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+Challenge: 
+
+- Add some styling! Be creative, play with layout a bit
+  (Spoiler: I'm going to use a single-column flexbox layout)
+  add some color, grab a font from Google fonts - whatever
+  you'd like!
+*/
+
+// fetch("https://apis.scrimba.com/bored/api/activity")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         document.getElementById("activity-name").textContent = data.activity
+//     })
